dc468d82f81b49215b5094982f59764cefa4ad2d
Nivang Goyal <nivang231@gmail.com>
2026-03-29 23:30:29 +0530

    Initial Commit DB Connected

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+
+async function connectToDB() {
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("Connect to Database")
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+module.exports = connectToDB
